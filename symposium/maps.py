40b7f1d467a42892f89aa69a1427dae5cfbf65c8
Naman Shah <80583973+navronaman@users.noreply.github.com>
2024-07-23 17:37:15 -0400

    Initial commit for maps.py

---
+++ symposium/maps.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+# Path to NJ county shape file
+nj_counties = gpd.read_file(r"symposium\County_Boundaries_of_NJ\County_Boundaries_of_NJ.shp")
+print(nj_counties.head())
+print(nj_counties.shape)
+
+# Load the biomass data
+biomass_data = pd.read_csv(r"backend\fermentation\biomass_imperial.csv")
+# Remove the last row from the biomass_data DataFrame
+biomass_data = biomass_data.drop(biomass_data.index[-1])
+biomass_data["COUNTY"] = biomass_data["County"].str.upper()
+print(biomass_data.head())
+print(biomass_data.shape)
+
+nj_counties_merged = nj_counties.merge(biomass_data, on='COUNTY')
+print(nj_counties_merged.head())
+print(nj_counties_merged.shape)
+
+colormap = plt.get_cmap("YlOrRd")  # Adjust 'YlOrRd' for your preference
+
+fig, ax = plt.subplots(figsize=(10, 6))  # Adjust figure size as needed
+nj_counties_merged.plot(column='Annual Ethanol (gal/yr)', cmap=colormap, ax=ax, legend=True)
+
+# Add title, labels, etc.
+plt.title("NJ County Annual Ethanol Production (gal/yr)")
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.show()
+
+
+fig, ax = plt.subplots(figsize=(10, 6))  # Adjust figure size as needed
+nj_counties_merged.plot(column='Annual Ethanol (gal/yr)', cmap=colormap, ax=ax, legend=True)
+
+# Add title, labels, etc.
+plt.title("NJ County Annual Ethanol Production (gal/yr)")
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.show()
